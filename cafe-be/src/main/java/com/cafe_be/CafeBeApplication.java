c079d3958ef928cb553ad72f6ec7aa77bcf5bf26
Tranquynh-2135 <do53231@donga.edu.vn>
2025-10-22 10:22:40 +0700

    Init project

---
+++ cafe-be/src/main/java/com/cafe_be/CafeBeApplication.java
@@ -0,0 +1,13 @@
+package com.cafe_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeBeApplication.class, args);
+	}
+
+}
